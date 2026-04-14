c1fafc14d1ebb7d3f7cf5d5dd1750b7704910488
S-Souza-cloud <silascalebe5@gmail.com>
2026-04-13 21:37:24 -0300

    Add files via upload

---
+++ Atividade dia 13/JavaApplication1/src/javaapplication1/JavaApplication1.java
@@ -0,0 +1,61 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package javaapplication1;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author 326114045
+ */
+public class JavaApplication1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+      Scanner scanner = new Scanner(System.in);
+
+        
+        System.out.print("Digite o número de linhas (n): ");
+        int n = scanner.nextInt();
+
+        System.out.print("Digite o número de colunas (m): ");
+        int m = scanner.nextInt();
+
+        int[][] matriz = new int[n][m];
+
+       
+        System.out.println("Digite os elementos da matriz:");
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                matriz[i][j] = scanner.nextInt();
+            }
+        }
+
+       
+        System.out.println("\nMatriz original:");
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                System.out.print(matriz[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+        
+        System.out.println("\nMatriz transposta:");
+        for (int j = 0; j < m; j++) {
+            for (int i = 0; i < n; i++) {
+                System.out.print(matriz[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+        scanner.close();
+    }
+}   
+    
+    
+
